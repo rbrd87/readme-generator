73f431b59cf9d8dde0c35f6b1ff6b97dcbe0cb90
rbrd87 <rochellembradley@gmail.com>
2023-02-17 22:30:34 +0000

    Added a shortname for the licenses which is used in the badge, added path.join to add the readme file to the correct folder, added comments throughout

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// packages required for the application
 const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
+
+// link to the generateMarkdown javascript file
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// array of questions for user
+// array of questions for user to give project data to generate the readme file
 const questions = [
     {
+        // validation added to prevent blank entry
         type: 'input',
         message: 'What is the title of your project?',
         name: 'title',
@@ -30,6 +34,7 @@ const questions = [
         }
     },
     {
+        // default value given so that the user doesn't give a blank entry
         type: 'input',
         message: 'Please add some installation instructions for your project',
         name: 'installation',
@@ -42,23 +47,24 @@ const questions = [
         default: 'N/A'
     },
     {
+        // list of GitHub licenses for the user to choose from
         type: 'list',
         message: 'Please enter a license for your project',
         name: 'license',
         choices: [
-            'GNU Affero General Public License v3.0',
-            'Apache license 2.0',
-            'BSD 2-clause "Simplified" license',
-            'BSD 3-clause "New" or "Revised" license',
-            'Boost Software License 1.0',
-            'Creative Commons Zero v1.0 Universal',
-            'Eclipse Public License 2.0',
-            'GNU General Public License v2.0',
-            'GNU General Public License v3.0',
-            'GNU Lesser General Public License v2.1',
-            'MIT',
-            'Mozilla Public License 2.0',
-            'The Unlicense',
+            'GNU Affero General Public License v3.0 / agpl-3.0',
+            'Apache license 2.0 / apache-2.0',
+            'BSD 2-clause "Simplified" license / bsd-2-clause',
+            'BSD 3-clause "New" or "Revised" license / bsd-3-clause',
+            'Boost Software License 1.0 / bsl-1.0',
+            'Creative Commons Zero v1.0 Universal / cc0-1.0',
+            'Eclipse Public License 2.0 / epl-2.0',
+            'GNU General Public License v2.0 / gpl-2.0',
+            'GNU General Public License v3.0 / gpl-3.0',
+            'GNU Lesser General Public License v2.1 / lgpl-2.1',
+            'MIT License / mit',
+            'Mozilla Public License 2.0 / mpl-2.0',
+            'The Unlicense / unlicense',
             'None'
         ]
     },
@@ -101,17 +107,19 @@ const questions = [
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
+const writeToFile = (fileName, data) => {
     fs.writeFile(fileName, generateMarkdown(data), (error) =>
-        (error ? console.log(error) : console.log('Success!'))
+        (error ? console.log(error) : console.log(`${data.title}-README.md file has been written`))
     );
 }
 
 // function to initialize program
-function init() {
+const init = () => {
     inquirer.prompt(questions).then((answers) => {
         //console.log(answers);
-        writeToFile('new-readme.md', answers);
+
+        // using path.join I can specify that the readme is saved in the correct folder
+        writeToFile(path.join ('./readme-files/', `${answers.title}-README.md`), answers);
     });
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,12 +1,25 @@
-const licenseColors = ["brightgreen", "green", "yellowgreen", "yellow", "orange", "red", "blue", "deepskyblue", "blueviolet", "hotpink"];
-const randomColor = licenseColors[Math.floor(Math.random() * licenseColors.length)];
+// array of various colours that can be used for the license badged
+const licenseColours = ["red", "yellow", "hotpink", "green", "blueviolet", "orange", "blue"];
+// randomColour picker so the same colour isn't used all the time
+const randomColour = licenseColours[Math.floor(Math.random() * licenseColours.length)];
+
+// empty array to store some license name data 
+let licenseArray = [];
+let licenseName = "";
+
 
 // function to generate a license badge based on the users input
 // if user selects 'None' no badge will be generated
-function getLicenseBadge(license) {
+const getLicenseBadge = (license) => {
+  // splitting the license by the '/' so that the shortname can be used for the badge
+  licenseArray = license.split('/')
+  licenseName = licenseArray[0]
+  
+  let licenseShortName = licenseArray[1].split(' ').join('')
+
   if (license != 'None') {
     //console.log(license);
-    return `![${license} badge](https://img.shields.io/static/v1?label=license&message=${license.split(' ').join('')}&color=${randomColor}&style=flat)`;
+    return `![${licenseName} badge](https://img.shields.io/static/v1?label=license&message=${licenseShortName}&color=${randomColour}&style=flat)`;
   } else {
     return "";
   }
@@ -14,16 +27,16 @@ function getLicenseBadge(license) {
 
 // function to populate the license section based on the users input
 // if the user selects 'None' then a message to state no license is being used is shown
-function setLicenseText(license) {
+const setLicenseText = (license) => {
   if (license != 'None') {
-    return `This project is covered by ${license}`;
+    return `This project is licensed under the terms of ${licenseName}`;
   } else {
     return "No license used for this project";
   }
 };
 
 // function to generate markdown for README
-function generateMarkdown(data) {
+const generateMarkdown = (data) => {
   return `# ${data.title}
 
 ${getLicenseBadge(data.license)}
@@ -63,4 +76,5 @@ Email - [${data.email}](mailto:${data.email}?subject=${data.title})
 ${setLicenseText(data.license)}`;
 };
 
+// exporting the functions to use in index.js
 module.exports = generateMarkdown;
