643b48b93584b9b744842d4c83d144a283d68713
rbrd87 <rochellembradley@gmail.com>
2023-02-17 18:08:04 +0000

    Worked out the write to file function and added the inquirer questions to the init function

--- index.js
+++ index.js
@@ -5,16 +5,26 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), (error) =>
+        (error ? console.log(error) : console.log("Success!"))
+    );
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then((answers) => {
+        //console.log(answers);
+        writeToFile("new-readme.md", answers);
+    });
 }
 
 // function call to initialize program
---
+++ new-readme.md
@@ -0,0 +1,2 @@
+# Rochelle's Project
+
